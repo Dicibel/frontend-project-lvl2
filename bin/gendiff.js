623e837a1c1844e810e9b446b456942ab09c4fac
Dmitriy Belyaev <dicibeel@gmail.com>
2020-05-15 21:31:31 +0200

    add "output format"

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,14 @@
 #!/usr/bin/env node
 
-import gendiff from '../src/index.js';
+import program from 'commander';
+import genDiff from '../src/index.js';
 
-gendiff();
+program
+  .version('-V, --version')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filePath1> <filePath2>')
+  .action((filePath1, filePath2) => {
+    console.log(genDiff(filePath1, filePath2));
+  });
+program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-export default () => {
-  console.log('ddd');
+export default (pathFile1, pathFile2) => {
+  console.log(pathFile1);
+  console.log(pathFile2);
 };
