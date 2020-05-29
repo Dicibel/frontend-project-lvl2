7129e0bd2b396309a391fb3895c74c1613b0ffbb
Dmitriy Belyaev <dicibeel@gmail.com>
2020-05-29 14:09:32 +0200

    turn 3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
-export default (pathFile1, pathFile2) => {
-  console.log(pathFile1);
-  console.log(pathFile2);
+import fs from 'fs';
+
+export default (firstPathFile, secondPathFile) => {
+  const firstJsonFile = fs.readFileSync(firstPathFile, 'utf8');
+  const secondJsonFile = fs.readFileSync(secondPathFile, 'utf8');
+
+  const objFromFirstJsonFile = JSON.parse(firstJsonFile);
+  const keysFromFirstObj = Object.keys(objFromFirstJsonFile);
+
+  const objFromSecondJsonFile = JSON.parse(secondJsonFile);
+  const keysFromSecondObj = Object.keys(objFromSecondJsonFile);
+
+  const allKeysFromObjects = [...new Set(keysFromFirstObj.concat(keysFromSecondObj))];
+
+  const changes = allKeysFromObjects.map((key) => {
+    const valueFromFirstObj = objFromFirstJsonFile[key];
+    const valueFromSecondObj = objFromSecondJsonFile[key];
+    if (valueFromFirstObj === undefined) {
+      return `+ ${key}: ${valueFromSecondObj}`;
+    } else if (valueFromSecondObj === undefined) {
+      return `- ${key}: ${valueFromFirstObj}`;
+    } else if (valueFromFirstObj === valueFromSecondObj) {
+      return `  ${key}: ${valueFromSecondObj}`;
+    }
+    return `- ${key}: ${valueFromFirstObj}\n+ ${key}: ${valueFromSecondObj}`;
+  });
+  const result  = changes.join('\n');
+  return `{\n${result}\n}`;
 };
